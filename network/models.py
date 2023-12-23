55afa2f4b3db66611a9ab4aa67b59ee8c07e5e81
Mj <142387440+MjDamo@users.noreply.github.com>
2023-12-23 12:35:56 -0800

    finish models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,22 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    postBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted_by')
+    postContent = models.TextField()
+    postDate = models.DateTimeField(auto_now_add=True)
+    postLikes = models.IntegerField(default=0)
+    likedBy = models.ManyToManyField(User, related_name='liked_by', blank=True)
+
+    def __str__(self):
+        return f"Posted by: {self.postBy}, Date: {self.postDate}, Likes: {self}"
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+    def __str__(self):
+        return f"Follower: {self.follower}, Following: {self.following}"
